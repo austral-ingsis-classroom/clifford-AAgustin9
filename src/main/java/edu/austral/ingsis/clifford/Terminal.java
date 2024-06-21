b138c1df27043f75ada982a56490685ba941ee1b
AAgustin9 <128090168+AAgustin9@users.noreply.github.com>
2024-06-20 22:11:37 -0300

    start on impl of commands

--- src/main/java/edu/austral/ingsis/clifford/Terminal.java
+++ src/main/java/edu/austral/ingsis/clifford/Terminal.java
@@ -1,11 +1,27 @@
 package edu.austral.ingsis.clifford;
 
-import edu.austral.ingsis.clifford.commands.Command;
+import edu.austral.ingsis.clifford.commands.*;
 
 import java.util.Map;
 
 public class Terminal {
-    public final Map<String, Command> commands;
     public final Directory root;
+    public final Directory actualDir;
+    public final Map<String, Command> commands;
+
+
+    public Terminal(Directory root, Directory actualDir) {
+        this.commands = Map.of(
+                "cd", new Cd(this),
+                "ls", new Ls(this),
+                "touch", new Touch(this),
+                "mkdir", new Mkdir(this),
+                "rm", new Rm(this),
+                "pwd", new Pwd(this)
+        );
+        this.root = root;
+        this.actualDir = actualDir;
+    }
+
 
 }
--- src/main/java/edu/austral/ingsis/clifford/commands/Ls.java
+++ src/main/java/edu/austral/ingsis/clifford/commands/Ls.java
@@ -1,8 +1,16 @@
 package edu.austral.ingsis.clifford.commands;
 
+import edu.austral.ingsis.clifford.Terminal;
+
 import java.util.List;
 
 public class Ls implements Command{
+    private final Terminal terminal;
+
+    public Ls (Terminal terminal){
+        this.terminal = terminal;
+    }
+
     @Override
     public void run(List<String> flags, List<String> args) {
 
